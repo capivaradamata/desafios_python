308ffd561ecb580de2d326c7777857c97f883493
capivaradamata <astolforezende11@gmail.com>
2024-06-25 19:26:46 -0300

    Novos desafios
    25/06/2024

--- desafio061.py
+++ desafio061.py
@@ -6,3 +6,14 @@ while contagem != 10:
     contagem += 1
     resultado = resultado + razão
     print(f"{resultado}", end=";")
+print("Você gostaria de ver mais termos? S/N")
+resposta = str(input("Você gostaria de ver mais termos? Digite S/N")).upper()
+quantidade = int(input("Quanto termos a mais?"))
+total = 0
+if resposta == "S":
+    print("Esse são os proximos termos")
+    while total != quantidade:
+        total += 1
+        resultado = resultado + razão
+        print(f"{resultado}", end=":")
+else:print("Obriado!")
