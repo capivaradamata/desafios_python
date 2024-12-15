289deae168a4ccbd3610d1d8bcff32126ce2c1c3
capivaradamata <astolforezende11@gmail.com>
2024-12-15 20:25:28 -0300

    Create desafio072.py

---
+++ desafio072.py
@@ -0,0 +1,3 @@
+numero = int(input("Digite um numero entre 0 e 20:"))
+numeros_extenso = ("Zero", "Um","Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Onze", "Doze","Treze", "Quatorze", "Quinze", "Dezesseis", "dezessete", "Dezoito", "Dezenove", "Vinte")
+print(F"Você digitou o numero {numeros_extenso[numero]}")
